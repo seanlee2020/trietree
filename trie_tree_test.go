18ee92246bcbd7c124ae61fed4e29d25d2526ed0
seanl <seanl@scribd.com>
2020-08-13 10:51:16 -0700

    Add sort by score func

--- trie_tree_test.go
+++ trie_tree_test.go
@@ -2,6 +2,7 @@ package trie
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
@@ -15,7 +16,9 @@ func testTrieTree(t *testing.T, trie *TrieTree) {
 	q2 := "harry potter"
 	q3 := "harry potter book"
 	q4 := "harry potter movie"
+	q5 := "harry potter author"
 
+	trie.Insert(q5, 6, 10, 30)
 	trie.Insert(q1, 100, 30, 100)
 	trie.Insert(q2, 9, 20, 200)
 	trie.Insert(q3, 40, 50, 100)
@@ -38,10 +41,43 @@ func testTrieTree(t *testing.T, trie *TrieTree) {
 	fmt.Print("\nsize of nodeList is", len(nodeList))
 
 	for idx, node := range nodeList {
-
 		fmt.Print("\nidx=", idx)
 		fmt.Print("\nnode.token=", node.token)
-
 	}
 
+	sortNodes(nodeList)
+
+	for idx, node := range nodeList {
+		fmt.Print("\nidx=", idx)
+		fmt.Print("\nnode.token=", node.token)
+	}
+
+}
+
+func sortNodes(nodeList []*TrieNode) {
+	sort.SliceStable(nodeList, func(i, j int) bool {
+		nodeI, nodeJ := nodeList[i], nodeList[j]
+		scoreI := nodeI.numUsers*10 + nodeI.numSessions*3
+		scoreJ := nodeJ.numUsers*10 + nodeJ.numSessions*3
+		return scoreI > scoreJ
+	})
+
+}
+
+type Member struct {
+	Id        int
+	LastName  string
+	FirstName string
+}
+
+func sortByLastNameAndFirstName(members []Member) {
+	sort.SliceStable(members, func(i, j int) bool {
+		mi, mj := members[i], members[j]
+		switch {
+		case mi.LastName != mj.LastName:
+			return mi.LastName < mj.LastName
+		default:
+			return mi.FirstName < mj.FirstName
+		}
+	})
 }
